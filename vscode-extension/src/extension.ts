848e98c4e8775aa9aa49f0deb1aa9803eebef4c8
Matan Zruya <mzruya@gmail.com>
2022-06-26 12:50:34 -0400

    prettify js

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,29 +1,34 @@
-import * as vscode from 'vscode';
-import { copyReference } from 'ast-parser';
+import * as vscode from "vscode";
+import { copyReference } from "ast-parser";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('copy-ruby-reference.copy-reference', () => {
-		const editor = vscode.window.activeTextEditor;
-		const filePath = editor?.document.fileName;
-		const selection = editor?.selection;
+  let disposable = vscode.commands.registerCommand(
+    "copy-ruby-reference.copy-reference",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      const filePath = editor?.document.fileName;
+      const selection = editor?.selection;
 
-		if (selection && filePath) {
-			const line = selection.start.line + 1;
-			const character = selection.start.character + 1;
-			const reference = copyReference(filePath, line, character);
+      if (selection && filePath) {
+        const line = selection.start.line + 1;
+        const character = selection.start.character + 1;
+        const reference = copyReference(filePath, line, character);
 
-			if (reference !== "null") {
-				vscode.env.clipboard.writeText(reference);
-				vscode.window.showInformationMessage(`Copied to clipboard!`);
-			} else {
-				vscode.window.showInformationMessage(`Couldn't find a constant definition!`);
-			}
-		} else {
-			vscode.window.showInformationMessage(`Invalid selection!`);
-		}
-	});
+        if (reference !== "null") {
+          vscode.env.clipboard.writeText(reference);
+          vscode.window.showInformationMessage(`Copied to clipboard!`);
+        } else {
+          vscode.window.showInformationMessage(
+            `Couldn't find a constant definition!`
+          );
+        }
+      } else {
+        vscode.window.showInformationMessage(`Invalid selection!`);
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
